78dcaccfb7a8361348839d82e671f41807e0b450
Lildan <Lildan@Lildans-MacBook-Pro.local>
2017-11-29 23:47:30 +0200

    Added constants and f function

--- Lab1/main.go
+++ Lab1/main.go
@@ -1,7 +1,10 @@
 package main
 
-const t0 = 1.0
-const T = 6.0
+import "math"
+
+const t0 = -1.0
+const T = 2*math.Pi - 1
+const u0 = 8
 const eps = 0.000001
 const tau0 = 0.5
 const epsM = 0.000001
@@ -10,6 +13,12 @@ func main() {
 	println("Hi Lildan!")
 }
 
-func f() float64 {
-	return t0
+// u'(t) = f(t,u)
+func f(t float64, u float64) float64 {
+	return math.Pow(math.E, t0)*math.Pow(math.E, (1-math.Cos(t+1))) + math.Sin(t+1)*u
+}
+
+// Analytic solution of this differential equation found in Maple
+func u(t float64) float64 {
+	return 42.0
 }
